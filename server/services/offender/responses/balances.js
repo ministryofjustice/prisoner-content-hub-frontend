b4ab4cad715b5838d0b118b0dfc4780ffb7ff3ea
Lucas Cairns <lucas.cairns@gmail.com>
2020-08-26 17:17:19 +0100

    Refactor Balances

---
+++ server/services/offender/responses/balances.js
@@ -0,0 +1,37 @@
+const DEFAULT = 'Unavailable';
+
+class Balances {
+  constructor(options = {}) {
+    const { spends, cash, savings, currency } = options;
+    this.spends = spends;
+    this.cash = cash;
+    this.savings = savings;
+    this.currency = currency;
+  }
+
+  format() {
+    const formatBalance = (amount, currency) => {
+      return amount && currency
+        ? new Intl.NumberFormat('en-GB', {
+            style: 'currency',
+            currency,
+          }).format(amount)
+        : DEFAULT;
+    };
+
+    return {
+      spends: formatBalance(this.spends, this.currency),
+      cash: formatBalance(this.cash, this.currency),
+      savings: formatBalance(this.savings, this.currency),
+      currency: this.currency || DEFAULT,
+    };
+  }
+
+  static from(response = {}) {
+    return new Balances(response);
+  }
+}
+
+module.exports = {
+  Balances,
+};
