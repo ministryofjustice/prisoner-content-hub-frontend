9f67b747cba95fb1b3fefd5a236ad760133adfae
Lucas Cairns <lucas.cairns@gmail.com>
2020-08-27 17:14:23 +0100

    Add returnUrl behaviour to auth callback page

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,9 +4,22 @@ const passport = require('passport');
 
 const createAuthRouter = () => {
   const router = express.Router();
-  router.get('/sign-in', passport.authenticate('azure_ad_oauth2'));
-  router.get('/provider/callback', passport.authenticate('azure_ad_oauth2', { successRedirect: '/' }));
 
+  router.get('/sign-in', (req, res, next) => {
+    if (req.query.returnUrl) { req.session.returnUrl = req.query.returnUrl; }
+    passport.authenticate('azure_ad_oauth2')(req, res, next);
+  });
+
+  router.get('/provider/callback', (req, res, next) => {
+    passport.authenticate('azure_ad_oauth2', (err, user, info) => {
+      if (err) { return next(err); }
+      if (!user) { return res.redirect('/auth/sign-in'); }
+      req.logIn(user, err => {
+        if (err) { return next(err); }
+        return res.redirect(req.session.returnUrl || '/');
+      })
+    })(req, res, next);
+  });
 
   router.get('/sign-out', (req, res) => res.send('TODO'));
 
