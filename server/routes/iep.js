348296e953cac17f8dfc4505b9b8985a09b218cb
Lucas Cairns <lucas.cairns@gmail.com>
2020-08-28 16:16:39 +0100

    Fix integrations on IEP page

--- server/routes/iep.js
+++ server/routes/iep.js
@@ -5,27 +5,29 @@ const createIepRouter = ({ hubContentService, offenderService, logger }) => {
   const router = express.Router();
 
   router.get('/', async (req, res, next) => {
-    const id = 4204;
-
     logger.info('GET /iep');
-
-    const userName = path(['session', 'user', 'name'], req);
-    const bookingId = path(['session', 'user', 'bookingId'], req);
-    const establishmentId = path(['locals', 'establishmentId'], res);
-    const config = {
-      content: true,
-      header: false,
-      postscript: true,
-      detailsType: 'small',
-      category: 'iep',
-      userName,
-      returnUrl: req.originalUrl,
-    };
-
     try {
-      const iep = await offenderService.getIEPSummaryFor(bookingId);
+      const id = 4204;
+      const establishmentId = path(['locals', 'establishmentId'], res);
+
       const data = await hubContentService.contentFor(id, establishmentId);
-      data.personalisedData = iep;
+
+      const config = {
+        content: true,
+        header: false,
+        postscript: true,
+        detailsType: 'small',
+        category: 'iep',
+        returnUrl: req.originalUrl,
+      };
+
+      if (req.user) {
+        const userName = req.user && req.user.getFullName();
+        const { bookingId } = req.user;
+        const iep = await offenderService.getIEPSummaryFor(bookingId);
+        data.personalisedData = iep;
+        config.userName = userName;
+      }
 
       return res.render('pages/category', {
         title: 'Incentives',
