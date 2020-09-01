c868f2f8c2ed312e65160c93b24d3bcb8799a0ff
Lucas Cairns <lucas.cairns@gmail.com>
2020-09-01 10:25:26 +0100

    Create initial refactor of timetable response handling (#75)

--- server/services/offender/index.js
+++ server/services/offender/index.js
@@ -7,66 +7,11 @@ const { Offender } = require('./responses/offender');
 const { KeyWorker } = require('./responses/keyWorker');
 const { NextVisit } = require('./responses/nextVisit');
 const { ImportantDates } = require('./responses/importantDates');
+const { TimeTable } = require('./responses/timeTable');
 
 const prettyTime = date => {
   if (!isValid(new Date(date))) return '';
   return format(parseISO(date), 'h:mmaaa');
-};
-
-const isoDate = date => {
-  if (!isValid(new Date(date))) return '';
-  return format(parseISO(date), 'yyyy-MM-dd');
-};
-
-const getTimetableTitle = date => {
-  const givenDate = new Date(date);
-
-  if (!isValid(givenDate)) return '';
-
-  const today = new Date();
-  const tomorrow = addDays(today, 1);
-  const todayDateString = format(today, 'EEEE d MMMM');
-  const tomorrowDateString = format(tomorrow, 'EEEE d MMMM');
-  const givenDateString = format(givenDate, 'EEEE d MMMM');
-
-  if (givenDateString === todayDateString) {
-    return 'Today';
-  }
-
-  if (givenDateString === tomorrowDateString) {
-    return 'Tomorrow';
-  }
-
-  return givenDateString;
-};
-
-const getTimetableEventTime = (startTime, endTime) => {
-  if (startTime === '') {
-    return '';
-  }
-
-  if (endTime !== '') {
-    return `${startTime} to ${endTime}`;
-  }
-
-  return startTime;
-};
-
-const getTimeOfDay = date => {
-  const dateObject = new Date(date);
-  if (!isValid(dateObject)) return '';
-
-  const dateString = isoDate(date);
-
-  if (isBefore(dateObject, parseISO(`${dateString} 12:00:00`))) {
-    return 'morning';
-  }
-
-  if (isBefore(dateObject, parseISO(`${dateString} 17:00:00`))) {
-    return 'afternoon';
-  }
-
-  return 'evening';
 };
 
 const createOffenderService = repository => {
@@ -130,6 +75,7 @@ const createOffenderService = repository => {
     }
   }
 
+  // Move to the repository Tier?
   async function getActualHomeEvents(bookingId, time) {
     const hour = Number.parseInt(format(time, 'H'), 10);
     const tomorrowCutOffHour = 19;
@@ -190,41 +136,6 @@ const createOffenderService = repository => {
     }
   }
 
-  const getInitialEvents = (startDate, endDate) => {
-    let checkDateObj = new Date(startDate);
-    let checkDateStr = format(checkDateObj, 'yyyy-MM-dd');
-    const endDateObj = new Date(endDate);
-    const endDateStr = format(endDateObj, 'yyyy-MM-dd');
-    const todayObj = new Date();
-    const events = {};
-
-    while (checkDateStr !== endDateStr) {
-      checkDateStr = format(checkDateObj, 'yyyy-MM-dd');
-
-      const finished = isBefore(checkDateObj, todayObj);
-
-      events[checkDateStr] = {
-        morning: {
-          finished,
-          events: [],
-        },
-        afternoon: {
-          finished,
-          events: [],
-        },
-        evening: {
-          finished,
-          events: [],
-        },
-        title: getTimetableTitle(checkDateStr),
-      };
-
-      checkDateObj = addDays(checkDateObj, 1);
-    }
-
-    return events;
-  };
-
   async function getEventsFor(bookingId, startDate, endDate) {
     try {
       const startDateObj = new Date(startDate);
@@ -238,73 +149,25 @@ const createOffenderService = repository => {
         throw new Error('Start date is after end date');
       }
 
-      const events = getInitialEvents(startDate, endDate);
+      const timeTable = TimeTable.forRange(startDate, endDate);
 
-      if (bookingId) {
-        const eventsData = await repository.getEventsFor(
-          bookingId,
-          startDate,
-          endDate,
-        );
+      const eventsData = await repository.getEventsFor(
+        bookingId,
+        startDate,
+        endDate,
+      );
 
-        if (!Array.isArray(eventsData)) {
-          throw new Error('Invalid data returned from API');
-        }
-
-        eventsData.forEach(event => {
-          const startTime = prettyTime(prop('startTime', event));
-          const endTime = prettyTime(prop('endTime', event));
-          const dateString = isoDate(prop('startTime', event));
-          const timeOfDay = getTimeOfDay(prop('startTime', event));
-
-          events[dateString][timeOfDay].events.push({
-            description: event.eventSourceDesc,
-            startTime,
-            endTime,
-            location: capitalize(event.eventLocation),
-            timeString: getTimetableEventTime(startTime, endTime),
-            eventType: event.eventType,
-            finished: event.eventStatus !== 'SCH',
-            status: event.eventStatus,
-            paid: event.paid,
-          });
-
-          if (event.eventStatus === 'SCH') {
-            events[dateString][timeOfDay].finished = false;
-          }
-        });
+      if (!Array.isArray(eventsData)) {
+        throw new Error('Invalid data returned from API');
       }
 
-      return setDayBlocks(events);
+      eventsData.forEach(timeTable.addEvent.bind(timeTable));
+      return timeTable.setEventStatesForToday().build();
     } catch {
       return {
         error: `We are not able to show your schedule for the selected week at this time`,
       };
     }
-  }
-
-  function setDayBlocks(events) {
-    /* eslint-disable no-param-reassign */
-    const nowDateString = format(new Date(), 'yyyy-MM-dd');
-
-    if (events[nowDateString]) {
-      const nowString = format(new Date(), 'yyyy-MM-dd HH:mm');
-      const currentTimeOfDay = getTimeOfDay(nowString);
-
-      events[nowDateString].morning.finished = false;
-      events[nowDateString].afternoon.finished = false;
-      events[nowDateString].evening.finished = false;
-
-      if (currentTimeOfDay === 'afternoon') {
-        events[nowDateString].morning.finished = true;
-      } else if (currentTimeOfDay === 'evening') {
-        events[nowDateString].morning.finished = true;
-        events[nowDateString].afternoon.finished = true;
-      }
-    }
-
-    return events;
-    /* eslint-enable no-param-reassign */
   }
 
   return {
