b4060d0e4068ceafe137157db0e978df53359d89
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-09-02 16:19:21 +0100

    Add more tests for analytics repository

--- test/repositories/analytics.spec.js
+++ test/repositories/analytics.spec.js
@@ -1,8 +1,9 @@
+const { expect } = require('chai');
 const { analyticsRepository } = require('../../server/repositories/analytics');
 
-describe.only('analyticsRepository', () => {
+describe('analyticsRepository', () => {
   describe('sendEvent', () => {
-    it('validates the offender number before making a call to the API', async () => {
+    it('should send all passed in data through to GA', async () => {
       const basicData = {
         category: 'category',
         action: 'action',
@@ -25,7 +26,214 @@ describe.only('analyticsRepository', () => {
       }
 
       expect(exception).to.be.null;
-      expect(client.postFormData.called).to.equal(true);
+      expect(client.postFormData.args[0][1].ec).to.equal(basicData.category);
+      expect(client.postFormData.args[0][1].ea).to.equal(basicData.action);
+      expect(client.postFormData.args[0][1].el).to.equal(basicData.label);
+      expect(client.postFormData.args[0][1].ev).to.equal(basicData.value);
+      expect(client.postFormData.args[0][1].cid).to.equal(basicData.sessionId);
+      expect(client.postFormData.args[0][1].ua).to.equal(basicData.userAgent);
+    });
+
+    it('should skip userAgent if not present', async () => {
+      const basicData = {
+        category: 'category',
+        action: 'action',
+        label: 'label',
+        value: 'value',
+        sessionId: 'sessionId',
+      };
+      const client = {
+        postFormData: sinon.stub(),
+      };
+      const repository = analyticsRepository(client);
+
+      let exception = null;
+
+      try {
+        await repository.sendEvent(basicData);
+      } catch (e) {
+        exception = e;
+      }
+
+      expect(exception).to.be.null;
+      expect(client.postFormData.args[0][1].ec).to.equal(basicData.category);
+      expect(client.postFormData.args[0][1].ea).to.equal(basicData.action);
+      expect(client.postFormData.args[0][1].el).to.equal(basicData.label);
+      expect(client.postFormData.args[0][1].ev).to.equal(basicData.value);
+      expect(client.postFormData.args[0][1].cid).to.equal(basicData.sessionId);
+      expect(client.postFormData.args[0][1].ua).to.equal(undefined);
+    });
+
+    it('should skip value if not present', async () => {
+      const basicData = {
+        category: 'category',
+        action: 'action',
+        label: 'label',
+        sessionId: 'sessionId',
+      };
+      const client = {
+        postFormData: sinon.stub(),
+      };
+      const repository = analyticsRepository(client);
+
+      let exception = null;
+
+      try {
+        await repository.sendEvent(basicData);
+      } catch (e) {
+        exception = e;
+      }
+
+      expect(exception).to.be.null;
+      expect(client.postFormData.args[0][1].ec).to.equal(basicData.category);
+      expect(client.postFormData.args[0][1].ea).to.equal(basicData.action);
+      expect(client.postFormData.args[0][1].el).to.equal(basicData.label);
+      expect(client.postFormData.args[0][1].ev).to.equal(undefined);
+      expect(client.postFormData.args[0][1].cid).to.equal(basicData.sessionId);
+    });
+  });
+  describe('sendPageTrack', () => {
+    it('should send all passed in data through to GA', async () => {
+      const basicData = {
+        hostname: 'hostname',
+        page: 'page',
+        title: 'title',
+        sessionId: 'sessionId',
+        userAgent: 'userAgent',
+        screen: 'screen',
+        viewport: 'viewport',
+        secondaryTags: 'secondaryTags',
+        categories: 'categories',
+      };
+      const client = {
+        postFormData: sinon.stub(),
+      };
+      const repository = analyticsRepository(client);
+
+      let exception = null;
+
+      try {
+        await repository.sendPageTrack(basicData);
+      } catch (e) {
+        exception = e;
+      }
+
+      expect(exception).to.be.null;
+      expect(client.postFormData.args[0][1].dh).to.equal(basicData.hostname);
+      expect(client.postFormData.args[0][1].dp).to.equal(basicData.page);
+      expect(client.postFormData.args[0][1].dt).to.equal(basicData.title);
+      expect(client.postFormData.args[0][1].cid).to.equal(basicData.sessionId);
+      expect(client.postFormData.args[0][1].ua).to.equal(basicData.userAgent);
+      expect(client.postFormData.args[0][1].sr).to.equal(basicData.screen);
+      expect(client.postFormData.args[0][1].vp).to.equal(basicData.viewport);
+      expect(client.postFormData.args[0][1].cd1).to.equal(
+        basicData.secondaryTags,
+      );
+      expect(client.postFormData.args[0][1].cd2).to.equal(basicData.categories);
+    });
+
+    it('should skip userAgent if not present', async () => {
+      const basicData = {
+        hostname: 'hostname',
+        page: 'page',
+        title: 'title',
+        sessionId: 'sessionId',
+        screen: 'screen',
+        viewport: 'viewport',
+        secondaryTags: 'secondaryTags',
+        categories: 'categories',
+      };
+      const client = {
+        postFormData: sinon.stub(),
+      };
+      const repository = analyticsRepository(client);
+
+      let exception = null;
+
+      try {
+        await repository.sendPageTrack(basicData);
+      } catch (e) {
+        exception = e;
+      }
+
+      expect(exception).to.be.null;
+      expect(client.postFormData.args[0][1].dh).to.equal(basicData.hostname);
+      expect(client.postFormData.args[0][1].dp).to.equal(basicData.page);
+      expect(client.postFormData.args[0][1].dt).to.equal(basicData.title);
+      expect(client.postFormData.args[0][1].cid).to.equal(basicData.sessionId);
+      expect(client.postFormData.args[0][1].ua).to.equal(undefined);
+      expect(client.postFormData.args[0][1].sr).to.equal(basicData.screen);
+      expect(client.postFormData.args[0][1].vp).to.equal(basicData.viewport);
+      expect(client.postFormData.args[0][1].cd1).to.equal(
+        basicData.secondaryTags,
+      );
+      expect(client.postFormData.args[0][1].cd2).to.equal(basicData.categories);
+    });
+
+    it('should skip secondaryTags if not present', async () => {
+      const basicData = {
+        hostname: 'hostname',
+        page: 'page',
+        title: 'title',
+        sessionId: 'sessionId',
+        screen: 'screen',
+        viewport: 'viewport',
+        categories: 'categories',
+      };
+      const client = {
+        postFormData: sinon.stub(),
+      };
+      const repository = analyticsRepository(client);
+
+      let exception = null;
+
+      try {
+        await repository.sendPageTrack(basicData);
+      } catch (e) {
+        exception = e;
+      }
+
+      expect(exception).to.be.null;
+      expect(client.postFormData.args[0][1].dh).to.equal(basicData.hostname);
+      expect(client.postFormData.args[0][1].dp).to.equal(basicData.page);
+      expect(client.postFormData.args[0][1].dt).to.equal(basicData.title);
+      expect(client.postFormData.args[0][1].cid).to.equal(basicData.sessionId);
+      expect(client.postFormData.args[0][1].sr).to.equal(basicData.screen);
+      expect(client.postFormData.args[0][1].vp).to.equal(basicData.viewport);
+      expect(client.postFormData.args[0][1].cd1).to.equal(undefined);
+      expect(client.postFormData.args[0][1].cd2).to.equal(basicData.categories);
+    });
+
+    it('should skip categories if not present', async () => {
+      const basicData = {
+        hostname: 'hostname',
+        page: 'page',
+        title: 'title',
+        sessionId: 'sessionId',
+        screen: 'screen',
+        viewport: 'viewport',
+      };
+      const client = {
+        postFormData: sinon.stub(),
+      };
+      const repository = analyticsRepository(client);
+
+      let exception = null;
+
+      try {
+        await repository.sendPageTrack(basicData);
+      } catch (e) {
+        exception = e;
+      }
+
+      expect(exception).to.be.null;
+      expect(client.postFormData.args[0][1].dh).to.equal(basicData.hostname);
+      expect(client.postFormData.args[0][1].dp).to.equal(basicData.page);
+      expect(client.postFormData.args[0][1].dt).to.equal(basicData.title);
+      expect(client.postFormData.args[0][1].cid).to.equal(basicData.sessionId);
+      expect(client.postFormData.args[0][1].sr).to.equal(basicData.screen);
+      expect(client.postFormData.args[0][1].vp).to.equal(basicData.viewport);
+      expect(client.postFormData.args[0][1].cd2).to.equal(undefined);
     });
   });
 });
