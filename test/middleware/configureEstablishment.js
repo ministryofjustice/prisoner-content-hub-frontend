a128a51a13b91c780869519d31b6762ec570f26b
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2020-09-02 11:33:43 +0100

    Fix problem with prison data (#79)
    - Middleware to put prison info into session etc was removed

---
+++ test/middleware/configureEstablishment.js
@@ -0,0 +1,49 @@
+const {
+  configureEstablishment,
+} = require('../../server/middleware/configureEstablishment');
+
+describe('configureEstablishment', () => {
+  const defaultPrison = 'wayland';
+
+  const defaultRequest = {
+    app: {
+      locals: {
+        config: {
+          establishmentName: defaultPrison,
+        },
+      },
+    },
+  };
+
+  const next = sinon.spy();
+
+  beforeEach(() => {
+    next.resetHistory();
+  });
+
+  it('should use the set the session from the config', () => {
+    const configureEstablishmentMiddleware = configureEstablishment();
+    const req = {
+      ...defaultRequest,
+      session: {
+        prison: '',
+      },
+    };
+
+    const res = { locals: {} };
+
+    configureEstablishmentMiddleware(req, res, next);
+
+    expect(req.session).to.have.property(
+      'prison',
+      defaultPrison,
+      'establishment should be persisted in the session',
+    );
+    expect(res.locals).to.have.property(
+      'establishmentName',
+      `HMP ${defaultPrison.slice(0, 1).toUpperCase() + defaultPrison.slice(1)}`,
+    );
+    expect(res.locals).to.have.property('establishmentId');
+    expect(next.called).to.equal(true, 'next should have been called');
+  });
+});
