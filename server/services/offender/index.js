405b662dcd75ace99a186a51f6b848ad373233e3
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-09-07 15:19:04 +0100

    Return getActualHomeEvents properly

--- server/services/offender/index.js
+++ server/services/offender/index.js
@@ -125,8 +125,8 @@ const createOffenderService = (
       }
 
       return {
-        events: await repository.getEventsForToday(bookingId),
-        isTomorrow: false,
+        events,
+        isTomorrow,
       };
     } catch (e) {
       return {
