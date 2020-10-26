67898ccb5781f4ee95c1149d67d505ef74fbca5e
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-10-26 12:06:05 +0000

    Revert previous testing commit

--- server/middleware/configureEstablishment.js
+++ server/middleware/configureEstablishment.js
@@ -1,5 +1,5 @@
 const { v4: uuid } = require('uuid');
-// const { pathOr } = require('ramda');
+const { pathOr } = require('ramda');
 const {
   getEstablishmentId,
   getEstablishmentFormattedName,
@@ -8,24 +8,24 @@ const {
 } = require('../utils');
 
 const configureEstablishment = () => (req, res, next) => {
-  // if (
-  //   req.session &&
-  //   (!req.session.id ||
-  //     !req.session.establishmentId ||
-  //     req.session.establishmentPersonalisationEnabled === undefined)
-  // ) {
-  //   const replaceUrl = /-prisoner-content-hub.*$/g;
-  //   const establishmentName = pathOr('wayland', ['headers', 'host'], req)
-  //     .split('.')[0]
-  //     .replace(replaceUrl, '');
-  const establishmentName = 'wayland';
-  req.session.id = uuid();
-  req.session.establishmentName = establishmentName;
-  req.session.establishmentId = getEstablishmentId(establishmentName);
-  req.session.establishmentPersonalisationEnabled = getEstablishmentPersonalisation(
-    req.session.establishmentId,
-  );
-  // }
+  if (
+    req.session &&
+    (!req.session.id ||
+      !req.session.establishmentId ||
+      req.session.establishmentPersonalisationEnabled === undefined)
+  ) {
+    const replaceUrl = /-prisoner-content-hub.*$/g;
+    const establishmentName = pathOr('wayland', ['headers', 'host'], req)
+      .split('.')[0]
+      .replace(replaceUrl, '');
+
+    req.session.id = uuid();
+    req.session.establishmentName = establishmentName;
+    req.session.establishmentId = getEstablishmentId(establishmentName);
+    req.session.establishmentPersonalisationEnabled = getEstablishmentPersonalisation(
+      req.session.establishmentId,
+    );
+  }
 
   res.locals.feedbackId = uuid();
   res.locals.establishmentDisplayName = `${getEstablishmentPrefix(
