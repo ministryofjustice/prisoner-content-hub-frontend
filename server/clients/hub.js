72282f3da13d0abb60b56f2b7cf72cfc1b1d086a
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-10-27 06:00:13 +0000

    Return PR to pre caching

--- server/clients/hub.js
+++ server/clients/hub.js
@@ -1,40 +1,32 @@
 const qs = require('querystring');
 const { baseClient } = require('./baseClient');
 const { logger } = require('../utils/logger');
-const { InMemoryCachingStrategy } = require('../utils/caching');
 
 class HubContentClient {
   constructor(client = baseClient) {
-    this.cache = new InMemoryCachingStrategy();
     this.client = client;
   }
 
-  async get(endpoint, { query, ...rest } = {}) {
+  get(endpoint, { query, ...rest } = {}) {
     const queryString = {
       _format: 'json',
       _lang: 'en',
       ...query,
     };
-    const cacheId = `${endpoint}?${qs.stringify(queryString)}`;
-    const cached = await this.cache.get(cacheId);
 
-    if (cached) {
-      return JSON.parse(cached);
-    }
-
-    try {
-      const res = await this.client.get(endpoint, {
-        params: queryString,
-        ...rest,
+    return this.client
+      .get(endpoint, { params: queryString, ...rest })
+      .then(res => {
+        logger.info(
+          `HubContentClient (GET) - ${endpoint}?${qs.stringify(queryString)}`,
+        );
+        return res.data;
+      })
+      .catch(e => {
+        logger.error(`HubContentClient (GET) - Failed: ${e.message}`);
+        logger.debug(e.stack);
+        return null;
       });
-      logger.info(`HubContentClient (GET) - ${cacheId}`);
-      await this.cache.set(cacheId, JSON.stringify(res.data), 300);
-      return res.data;
-    } catch (e) {
-      logger.error(`HubContentClient (GET) - Failed: ${e.message}`);
-      logger.debug(e.stack);
-      return null;
-    }
   }
 }
 
--- server/middleware/configureEstablishment.js
+++ server/middleware/configureEstablishment.js
@@ -1,4 +1,5 @@
 const { v4: uuid } = require('uuid');
+const { pathOr } = require('ramda');
 const {
   getEstablishmentId,
   getEstablishmentFormattedName,
@@ -7,14 +8,24 @@ const {
 } = require('../utils');
 
 const configureEstablishment = () => (req, res, next) => {
-  const establishmentName = 'wayland';
+  if (
+    req.session &&
+    (!req.session.id ||
+      !req.session.establishmentId ||
+      req.session.establishmentPersonalisationEnabled === undefined)
+  ) {
+    const replaceUrl = /-prisoner-content-hub.*$/g;
+    const establishmentName = pathOr('wayland', ['headers', 'host'], req)
+      .split('.')[0]
+      .replace(replaceUrl, '');
 
-  req.session.id = uuid();
-  req.session.establishmentName = establishmentName;
-  req.session.establishmentId = getEstablishmentId(establishmentName);
-  req.session.establishmentPersonalisationEnabled = getEstablishmentPersonalisation(
-    req.session.establishmentId,
-  );
+    req.session.id = uuid();
+    req.session.establishmentName = establishmentName;
+    req.session.establishmentId = getEstablishmentId(establishmentName);
+    req.session.establishmentPersonalisationEnabled = getEstablishmentPersonalisation(
+      req.session.establishmentId,
+    );
+  }
 
   res.locals.feedbackId = uuid();
   res.locals.establishmentDisplayName = `${getEstablishmentPrefix(
