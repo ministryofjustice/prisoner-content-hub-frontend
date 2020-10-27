9095e1f659e245be836a78bb2498cc3d9a87b32e
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-10-27 06:25:40 +0000

    Add basic in-memory caching to GET calls

--- server/clients/standard.js
+++ server/clients/standard.js
@@ -2,24 +2,35 @@ const qs = require('querystring');
 const { path } = require('ramda');
 const { baseClient } = require('./baseClient');
 const { logger } = require('../utils/logger');
+const { InMemoryCachingStrategy } = require('../utils/caching');
 
 class StandardClient {
   constructor(client = baseClient) {
     this.client = client;
+    this.cache = new InMemoryCachingStrategy();
   }
 
-  get(endpoint, { query, ...rest } = {}) {
-    return this.client
-      .get(endpoint, { params: query, ...rest })
-      .then(res => {
-        logger.info(`StandardClient (GET) ${endpoint}?${qs.stringify(query)}`);
-        return res.data;
-      })
-      .catch(e => {
-        logger.error(`StandardClient (GET) - Failed: ${e.message}`);
-        logger.debug(e.stack);
-        return null;
+  async get(endpoint, { query, ...rest } = {}) {
+    const cacheId = `${endpoint}?${qs.stringify(query)}`;
+    const cached = await this.cache.get(cacheId);
+
+    if (cached) {
+      return JSON.parse(cached);
+    }
+
+    try {
+      const res = await this.client.get(endpoint, {
+        params: query,
+        ...rest,
       });
+      logger.info(`StandardClient (GET) - ${cacheId}`);
+      await this.cache.set(cacheId, JSON.stringify(res.data), 300);
+      return res.data;
+    } catch (e) {
+      logger.error(`StandardClient (GET) - Failed: ${e.message}`);
+      logger.debug(e.stack);
+      return null;
+    }
   }
 
   post(endpoint, data) {
