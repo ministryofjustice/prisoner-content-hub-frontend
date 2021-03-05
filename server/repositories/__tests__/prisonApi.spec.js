faaf875cef2cfce0361caf135c5996c66ee81466
Lucas Cairns <lucas.cairns@gmail.com>
2021-03-05 14:13:07 +0000

    Refactor PrisonApi repository test to use Jest's inbuilt methods

--- server/repositories/__tests__/prisonApi.spec.js
+++ server/repositories/__tests__/prisonApi.spec.js
@@ -1,5 +1,4 @@
 const { PrisonApiRepository } = require('../prisonApi');
-const { lastCall } = require('../../../test/test-helpers');
 
 describe('PrisonApiRepository', () => {
   const client = { get: jest.fn() };
@@ -14,7 +13,7 @@ describe('PrisonApiRepository', () => {
     it('should return when the transactions request succeeds', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.resolve('API_RESPONSE'));
+      client.get.mockResolvedValue('API_RESPONSE');
 
       const response = await repository.getTransactionsFor(
         'A123BC',
@@ -23,12 +22,9 @@ describe('PrisonApiRepository', () => {
         new Date('2020-01-31'),
       );
 
-      expect(lastCall(client.get)[0]).toContain(
-        '/offenders/A123BC/transaction-history',
+      expect(client.get).toHaveBeenCalledWith(
+        'http://foo.bar/api/offenders/A123BC/transaction-history?account_code=spends&from_date=2020-01-01&to_date=2020-01-31',
       );
-      expect(lastCall(client.get)[0]).toContain('account_code=spends');
-      expect(lastCall(client.get)[0]).toContain('from_date=2020-01-01');
-      expect(lastCall(client.get)[0]).toContain('to_date=2020-01-31');
 
       expect(response).toBe('API_RESPONSE');
     });
@@ -36,95 +32,73 @@ describe('PrisonApiRepository', () => {
     it('should throw when no prisoner ID is passed', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.resolve('API_RESPONSE'));
+      client.get.mockResolvedValue('API_RESPONSE');
 
-      let hasThrown = false;
-
-      try {
-        await repository.getTransactionsFor(
+      await expect(
+        repository.getTransactionsFor(
           null,
           'spends',
           new Date('2020-01-01'),
           new Date('2020-01-31'),
-        );
-      } catch (e) {
-        hasThrown = true;
-        expect(client.get).not.toHaveBeenCalled();
-      }
+        ),
+      ).rejects.toThrow();
 
-      expect(hasThrown).toBe(true);
+      expect(client.get).not.toHaveBeenCalled();
     });
 
     it('should throw when no accountCode is passed', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.resolve('API_RESPONSE'));
+      client.get.mockResolvedValue('API_RESPONSE');
 
-      let hasThrown = false;
-
-      try {
-        await repository.getTransactionsFor(
+      await expect(
+        repository.getTransactionsFor(
           'A123BC',
           null,
           new Date('2020-01-01'),
           new Date('2020-01-31'),
-        );
-      } catch (e) {
-        hasThrown = true;
-        expect(client.get).not.toHaveBeenCalled();
-      }
+        ),
+      ).rejects.toThrow();
 
-      expect(hasThrown).toBe(true);
+      expect(client.get).not.toHaveBeenCalled();
     });
 
     it('should throw when no fromDate is passed', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.resolve('API_RESPONSE'));
+      client.get.mockResolvedValue('API_RESPONSE');
 
-      let hasThrown = false;
-
-      try {
-        await repository.getTransactionsFor(
+      await expect(
+        repository.getTransactionsFor(
           'A123BC',
           'spends',
           null,
           new Date('2020-01-31'),
-        );
-      } catch (e) {
-        hasThrown = true;
-        expect(client.get).not.toHaveBeenCalled();
-      }
-
-      expect(hasThrown).toBe(true);
+        ),
+      ).rejects.toThrow();
     });
 
     it('should throw when no toDate is passed', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.resolve('API_RESPONSE'));
+      client.get.mockResolvedValue('API_RESPONSE');
 
-      let hasThrown = false;
-
-      try {
-        await repository.getTransactionsFor(
+      await expect(
+        repository.getTransactionsFor(
           'A123BC',
           'spends',
           new Date('2020-01-01'),
           null,
-        );
-      } catch (e) {
-        hasThrown = true;
-        expect(client.get).not.toHaveBeenCalled();
-      }
+        ),
+      ).rejects.toThrow();
 
-      expect(hasThrown).toBe(true);
+      expect(client.get).not.toHaveBeenCalled();
     });
 
     it('should swallow the error and return nothing when the request fails', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.rejects('💥'));
+      client.get.mockRejectedValue('💥');
 
       const response = await repository.getTransactionsFor(
         'A123BC',
@@ -141,11 +115,13 @@ describe('PrisonApiRepository', () => {
     it('should return when the prison details request succeeds', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.resolve('API_RESPONSE'));
+      client.get.mockResolvedValue('API_RESPONSE');
 
       const response = await repository.getPrisonDetailsFor('TST');
 
-      expect(lastCall(client.get)[0]).toContain('/agencies/TST');
+      expect(client.get).toHaveBeenCalledWith(
+        'http://foo.bar/api/agencies/TST',
+      );
 
       expect(response).toBe('API_RESPONSE');
     });
@@ -153,24 +129,17 @@ describe('PrisonApiRepository', () => {
     it('should throw when no prisonId is passed', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.resolve('API_RESPONSE'));
+      client.get.mockResolvedValue('API_RESPONSE');
 
-      let hasThrown = false;
+      await expect(repository.getPrisonDetailsFor()).rejects.toThrow();
 
-      try {
-        await repository.getPrisonDetailsFor();
-      } catch (e) {
-        hasThrown = true;
-        expect(client.get).not.toHaveBeenCalled();
-      }
-
-      expect(hasThrown).toBe(true);
+      expect(client.get).not.toHaveBeenCalled();
     });
 
     it('should swallow the error and return nothing when the request fails', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.rejects('💥'));
+      client.get.mockRejectedValue('💥');
 
       const response = await repository.getPrisonDetailsFor('TST');
 
@@ -182,11 +151,13 @@ describe('PrisonApiRepository', () => {
     it('should return when the balances request succeeds', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.resolve('API_RESPONSE'));
+      client.get.mockResolvedValue('API_RESPONSE');
 
       const response = await repository.getBalancesFor('123456');
 
-      expect(lastCall(client.get)[0]).toContain('/bookings/123456/balances');
+      expect(client.get).toHaveBeenCalledWith(
+        'http://foo.bar/api/bookings/123456/balances',
+      );
 
       expect(response).toBe('API_RESPONSE');
     });
@@ -194,24 +165,17 @@ describe('PrisonApiRepository', () => {
     it('should throw when no bookingId is passed', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.resolve('API_RESPONSE'));
+      client.get.mockResolvedValue('API_RESPONSE');
 
-      let hasThrown = false;
+      await expect(repository.getBalancesFor()).rejects.toThrow();
 
-      try {
-        await repository.getBalancesFor();
-      } catch (e) {
-        hasThrown = true;
-        expect(client.get).not.toHaveBeenCalled();
-      }
-
-      expect(hasThrown).toBe(true);
+      expect(client.get).not.toHaveBeenCalled();
     });
 
     it('should swallow the error and return nothing when the request fails', async () => {
       const repository = new PrisonApiRepository({ client, apiUrl });
 
-      client.get.mockImplementation(() => Promise.rejects('💥'));
+      client.get.mockRejectedValue('💥');
 
       const response = await repository.getBalancesFor('123456');
 
