7b56f050f2735583a8619d81bb709860b3336eee
Lucas Cairns <lucas.cairns@gmail.com>
2021-03-08 14:38:03 +0000

    Extract formatting from route for transactions

--- server/routes/money.js
+++ server/routes/money.js
@@ -1,25 +1,7 @@
 const { path } = require('ramda');
 const express = require('express');
 const { subDays } = require('date-fns');
-const { formatBalanceOrDefault } = require('../utils/string');
-const { formatDateOrDefault } = require('../utils/date');
-
-function formatTransaction(t) {
-  return {
-    paymentDate: formatDateOrDefault('', 'd MMMM yyyy', t.entryDate),
-    balance: formatBalanceOrDefault(null, t.currentBalance / 100, t.currency),
-    moneyIn:
-      t.postingType === 'CR'
-        ? formatBalanceOrDefault(null, t.penceAmount / 100, t.currency)
-        : null,
-    moneyOut:
-      t.postingType === 'DR'
-        ? formatBalanceOrDefault(null, t.penceAmount / 100, t.currency)
-        : null,
-    paymentDescription: t.entryDescription,
-    prison: t.prison,
-  };
-}
+const { formatTransactionPageData } = require('./formatters');
 
 function getAccountCodeFor(accountType) {
   const accountCodes = {
@@ -29,11 +11,6 @@ function getAccountCodeFor(accountType) {
   };
 
   return accountCodes[accountType];
-}
-
-function formatBalanceFor(accountType, balances) {
-  const balance = balances[getAccountCodeFor(accountType)];
-  return formatBalanceOrDefault(null, balance, balances.currency);
 }
 
 const createMoneyRouter = ({
@@ -79,58 +56,48 @@ const createMoneyRouter = ({
 
   router.get('/transactions', async (req, res, next) => {
     try {
-      const config = {
-        content: false,
-        header: false,
-        postscript: true,
-        detailsType: 'small',
-        returnUrl: req.originalUrl,
+      const templateData = {
+        title: 'Your transactions',
+        config: {
+          content: false,
+          header: false,
+          postscript: true,
+          detailsType: 'small',
+          returnUrl: req.originalUrl,
+        },
       };
 
       const { user } = req;
-
-      const data = {};
 
       if (user) {
         const accountTypes = ['spends', 'private', 'savings'];
         const accountType = accountTypes.includes(req.query.accountType)
           ? req.query.accountType
           : accountTypes[0];
+        const accountCode = getAccountCodeFor(accountType);
 
         const toDate = new Date();
         const fromDate = subDays(toDate, 30);
 
-        const prisonerInformation = await prisonerInformationService.getTransactionInformationFor(
+        const transactionsData = await prisonerInformationService.getTransactionInformationFor(
           user,
-          getAccountCodeFor(accountType),
+          accountCode,
           fromDate,
           toDate,
         );
 
-        if (prisonerInformation) {
-          data.transactions = prisonerInformation.transactions.error
-            ? prisonerInformation.transactions
-            : prisonerInformation.transactions.map(formatTransaction);
-          data.balance = prisonerInformation.balances.error
-            ? prisonerInformation.balances
-            : {
-                amount: formatBalanceFor(
-                  accountType,
-                  prisonerInformation.balances,
-                ),
-              };
-        }
+        const prisonerInformation = formatTransactionPageData(
+          accountCode,
+          transactionsData,
+        );
+        prisonerInformation.selected = accountType;
+        prisonerInformation.accountTypes = accountTypes;
 
-        data.selected = accountType;
-        data.accountTypes = accountTypes;
-        config.userName = user.getFullName();
+        templateData.prisonerInformation = prisonerInformation;
+        templateData.config.userName = user.getFullName();
       }
 
-      return res.render('pages/transactions', {
-        title: 'Your transactions',
-        config,
-        data,
-      });
+      return res.render('pages/transactions', templateData);
     } catch (e) {
       return next(e);
     }
