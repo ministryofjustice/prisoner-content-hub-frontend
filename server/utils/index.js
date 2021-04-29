f71c908d45d7e907bac0deaca54a62c38748f9c4
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-04-29 17:33:22 +0100

    Fixing typo on private savings card

--- server/utils/index.js
+++ server/utils/index.js
@@ -3,24 +3,54 @@ const defaultEstablishmentData = require('../content/establishmentData');
 
 const isEmptyResponse = val => isEmpty(val) || isNil(val);
 
-const getEstablishmentId = (establishmentName, establishmentData = defaultEstablishmentData) => 
-parseInt(Object.keys(establishmentData).find( id => establishmentData?.[id]?.name === establishmentName), 10)
+const getEstablishmentId = (
+  establishmentName,
+  establishmentData = defaultEstablishmentData,
+) =>
+  parseInt(
+    Object.keys(establishmentData).find(
+      id => establishmentData?.[id]?.name === establishmentName,
+    ),
+    10,
+  );
 
-const getEstablishmentName = (id, establishmentData = defaultEstablishmentData) => establishmentData?.[id]?.name
+const getEstablishmentName = (
+  id,
+  establishmentData = defaultEstablishmentData,
+) => establishmentData?.[id]?.name;
 
-const getEstablishmentPrefix = (id, establishmentData = defaultEstablishmentData) => establishmentData?.[id]?.prefix ?? 'HMP'
+const getEstablishmentPrefix = (
+  id,
+  establishmentData = defaultEstablishmentData,
+) => establishmentData?.[id]?.prefix ?? 'HMP';
 
-const getEstablishmentFormattedName = (id, establishmentData = defaultEstablishmentData) => establishmentData?.[id]?.formattedName
+const getEstablishmentFormattedName = (
+  id,
+  establishmentData = defaultEstablishmentData,
+) => establishmentData?.[id]?.formattedName;
 
-const getEstablishmentUiId = (id, establishmentData = defaultEstablishmentData) => establishmentData?.[id]?.uuId
+const getEstablishmentUiId = (
+  id,
+  establishmentData = defaultEstablishmentData,
+) => establishmentData?.[id]?.uuId;
 
-const getEstablishmentHomepageLinks = (id, establishmentData = defaultEstablishmentData) => establishmentData?.[id]?.homePageLinks
+const getEstablishmentHomepageLinks = (
+  id,
+  establishmentData = defaultEstablishmentData,
+) => establishmentData?.[id]?.homePageLinks;
 
-const getEstablishmentHomepageLinksTitle = (id, establishmentData = defaultEstablishmentData) =>  establishmentData?.[id]?.homePageLinksTitle
+const getEstablishmentHomepageLinksTitle = (
+  id,
+  establishmentData = defaultEstablishmentData,
+) => establishmentData?.[id]?.homePageLinksTitle;
 
-const getEstablishmentPersonalisation = (id, establishmentData = defaultEstablishmentData) => establishmentData?.[id]?.personalInformation
+const getEstablishmentPersonalisation = (
+  id,
+  establishmentData = defaultEstablishmentData,
+) => establishmentData?.[id]?.personalInformation;
 
-const capitalize = (str = '') => str === '' ? '' : str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+const capitalize = (str = '') =>
+  str === '' ? '' : str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
 function capitalizeAll(input, separator = ' ') {
   if (input === '') return '';
@@ -40,9 +70,10 @@ function capitalizePersonName(input, separator = ' ') {
     .join(separator);
 }
 
-const fillContentItems = (contentItems = [], number = 4) => contentItems.length % number 
-  ? contentItems.concat(new Array( number - (contentItems.length % number )))
-  : contentItems
+const fillContentItems = (contentItems = [], number = 4) =>
+  contentItems.length % number
+    ? contentItems.concat(new Array(number - (contentItems.length % number)))
+    : contentItems;
 
 module.exports = {
   getEstablishmentId,
