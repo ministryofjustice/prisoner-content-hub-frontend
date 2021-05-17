718969074f249ac45b135f9fc309c9152a77f1ae
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-05-17 07:42:36 +0100

    bug/fix-search: Changing search type
    We were previously doing a term search on a text value.
    This has [known limitations](https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-term-query.html]
    
     This would only work for single word prisons. The text value would be processed during index to split by individual word and lower case whereas the terms query would pass the exact phrase.
    
    So when we searched for 'Cookham wood' it would fail to match the tokens 'cookham' and 'wood'.
    Luckily according to the index we could see that a separate  prison_name.keyword was also indexed which indexes the whole thing.
    
    This change moves to using the formatted name and the keyword field as these currently always match.
    We should see if drupal would allow us to index establishment id as going forward a keyword index on establishment id would be more robust.

--- server/services/__tests__/search.spec.js
+++ server/services/__tests__/search.spec.js
@@ -1,18 +1,32 @@
 const { createSearchService } = require('../search');
-const { lastCall } = require('../../../test/test-helpers');
 
 describe('SearchService', () => {
+  const searchRepository = {
+    find: jest.fn(),
+    typeAhead: jest.fn(),
+  };
+  const getEstablishmentFormattedName = jest
+    .fn()
+    .mockReturnValue('HMP Development');
+  let service;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    getEstablishmentFormattedName.mockReturnValue('HMP Development');
+
+    service = createSearchService({
+      searchRepository,
+      getEstablishmentFormattedName,
+    });
+  });
+
   describe('#find', () => {
     it('returns search results', async () => {
-      const searchRepository = {
-        find: jest.fn().mockResolvedValue(['result_1', 'result_2', 'result_3']),
-      };
-      const getEstablishmentName = jest.fn().mockReturnValue('HMP Development');
-
-      const service = createSearchService({
-        searchRepository,
-        getEstablishmentName,
-      });
+      searchRepository.find.mockResolvedValue([
+        'result_1',
+        'result_2',
+        'result_3',
+      ]);
 
       const result = await service.find({
         query: 'Test Query',
@@ -21,44 +35,22 @@ describe('SearchService', () => {
         from: 5,
       });
 
-      expect(searchRepository.find).toHaveBeenCalledTimes(
-        1,
-        'The repository should have been called',
-      );
+      expect(searchRepository.find).toHaveBeenCalledTimes(1);
 
-      expect(lastCall(searchRepository.find)[0]).toStrictEqual(
-        {
-          query: 'Test Query',
-          prison: 'HMP Development',
-          limit: 10,
-          from: 5,
-        },
-        'The repository should have been called with the correct arguments',
-      );
+      expect(searchRepository.find).toHaveBeenCalledWith({
+        query: 'Test Query',
+        prison: 'HMP Development',
+        limit: 10,
+        from: 5,
+      });
 
-      expect(getEstablishmentName).toHaveBeenCalledTimes(
-        1,
-        'The service should get the establishment name',
-      );
-
-      expect(lastCall(getEstablishmentName)[0]).toStrictEqual(123);
-
-      expect(Array.isArray(result)).toBe(
-        true,
-        'The service should return an array of results',
-      );
+      expect(getEstablishmentFormattedName).toHaveBeenCalledTimes(1);
+      expect(getEstablishmentFormattedName).toHaveBeenCalledWith(123);
+      expect(Array.isArray(result)).toBe(true);
     });
 
     it('returns empty when there is no query', async () => {
-      const searchRepository = {
-        find: jest.fn(),
-      };
-      const getEstablishmentName = jest.fn().mockReturnValue('HMP Development');
-
-      const service = createSearchService({
-        searchRepository,
-        getEstablishmentName,
-      });
+      searchRepository.find.mockResolvedValue([]);
 
       const results = await service.find({
         query: '',
@@ -67,30 +59,19 @@ describe('SearchService', () => {
         from: 5,
       });
 
-      expect(Array.isArray(results)).toBe(
-        true,
-        'The method should return an array',
-      );
-
-      expect(results.length).toBe(0, 'The array should be empty');
-
+      expect(Array.isArray(results)).toBe(true);
+      expect(results.length).toBe(0);
       expect(searchRepository.find).not.toHaveBeenCalled();
     });
   });
 
   describe('#typeAhead', () => {
     it('returns search results', async () => {
-      const searchRepository = {
-        typeAhead: jest
-          .fn()
-          .mockResolvedValue(['result_1', 'result_2', 'result_3']),
-      };
-      const getEstablishmentName = jest.fn().mockReturnValue('HMP Development');
-
-      const service = createSearchService({
-        searchRepository,
-        getEstablishmentName,
-      });
+      searchRepository.typeAhead.mockResolvedValue([
+        'result_1',
+        'result_2',
+        'result_3',
+      ]);
 
       const result = await service.typeAhead({
         query: 'Test Query',
@@ -98,43 +79,21 @@ describe('SearchService', () => {
         limit: 3,
       });
 
-      expect(searchRepository.typeAhead).toHaveBeenCalledTimes(
-        1,
-        'The repository should have been called',
-      );
+      expect(searchRepository.typeAhead).toHaveBeenCalledTimes(1);
 
-      expect(lastCall(searchRepository.typeAhead)[0]).toStrictEqual(
-        {
-          query: 'Test Query',
-          prison: 'HMP Development',
-          limit: 3,
-        },
-        'The repository should have been called with the correct arguments',
-      );
+      expect(searchRepository.typeAhead).toHaveBeenCalledWith({
+        query: 'Test Query',
+        prison: 'HMP Development',
+        limit: 3,
+      });
 
-      expect(getEstablishmentName).toHaveBeenCalledTimes(
-        1,
-        'The service should get the establishment name',
-      );
-
-      expect(lastCall(getEstablishmentName)[0]).toStrictEqual(123);
-
-      expect(Array.isArray(result)).toBe(
-        true,
-        'The service should return an array of results',
-      );
+      expect(getEstablishmentFormattedName).toHaveBeenCalledTimes(1);
+      expect(getEstablishmentFormattedName).toHaveBeenCalledWith(123);
+      expect(Array.isArray(result)).toBe(true);
     });
 
     it('returns empty when there is no query', async () => {
-      const searchRepository = {
-        typeAhead: jest.fn(),
-      };
-      const getEstablishmentName = jest.fn().mockReturnValue('HMP Development');
-
-      const service = createSearchService({
-        searchRepository,
-        getEstablishmentName,
-      });
+      searchRepository.typeAhead.mockResolvedValue([]);
 
       const results = await service.typeAhead({
         query: '',
@@ -142,14 +101,9 @@ describe('SearchService', () => {
         limit: 3,
       });
 
-      expect(Array.isArray(results)).toBe(
-        true,
-        'The method should return an array',
-      );
-
-      expect(results.length).toBe(0, 'The array should be empty');
-
       expect(searchRepository.typeAhead).not.toHaveBeenCalled();
+      expect(Array.isArray(results)).toBe(true);
+      expect(results.length).toBe(0);
     });
   });
 });
