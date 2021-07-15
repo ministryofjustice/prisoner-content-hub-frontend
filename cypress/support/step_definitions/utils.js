d419ff36009cdaa474623544d748743b803a1968
jonhindle <50403492+jonhindle@users.noreply.github.com>
2021-07-15 13:45:19 +0100

    Test/profile end to end tests (#310)
    * profile visits end to end tests

---
+++ cypress/support/step_definitions/utils.js
@@ -0,0 +1,28 @@
+const horizontalTableToObject = ({ rawTable }) => {
+  const [keys, ...rows] = rawTable;
+  return rows.map(values =>
+    keys.reduce((obj, key, i) => {
+      const value = values[i];
+      obj[key] = isNaN(value) ? value : Number(value);
+      return obj;
+    }, {}),
+  );
+};
+
+const verticalTableToObject = ({ rawTable }) => {
+  return rawTable.reduce((arr, [key, ...rawValues]) => {
+    rawValues.forEach((rawValue, i) => {
+      if (!arr[i]) arr[i] = {};
+      const obj = arr[i];
+      const value = isNaN(rawValue) ? rawValue : Number(rawValue);
+      if (obj[key])
+        if (Array.isArray(obj[key])) obj[key].push(value);
+        else obj[key] = [obj[key], value];
+      else obj[key] = value;
+      return obj;
+    });
+    return arr;
+  }, []);
+};
+
+export { horizontalTableToObject, verticalTableToObject };
