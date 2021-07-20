c8344254aee8af3d3bc4eda5f16d40685e704017
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-07-20 13:48:50 +0100

    no visits

--- server/auth/middleware.js
+++ server/auth/middleware.js
@@ -49,7 +49,7 @@ const createMockSignIn = ({ offenderService }) =>
   async function mockSignIn(req, res, next) {
     try {
       const user = new User({
-        prisonerId: 'G9374GG',
+        prisonerId: 'G2732GG',
         firstName: 'Test',
         lastName: 'User',
       });
