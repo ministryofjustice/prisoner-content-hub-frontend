951cdbf204cdfd4d5b5663f5e3c16bcd40c07887
David Thomas <david.thomas1@digital.justice.gov.uk>
2021-07-22 16:17:46 +0100

    Add-sentry-performance-management (#309)
    * Add-sentry-performance-management
    
    * Bumping dependencies
    
    Co-authored-by: Andrew Lee <1517745+andrewrlee@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const passport = require('passport');
 const AzureAdOAuth2Strategy = require('passport-azure-ad-oauth2');
 const { path: ramdaPath, pathOr } = require('ramda');
 const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
 const nunjucksSetup = require('./utils/nunjucksSetup');
 
 const { createIndexRouter } = require('./routes/index');
@@ -71,8 +72,28 @@ const createApp = ({
 
   // Set up Sentry before (almost) everything else, so we can
   // capture any exceptions during startup
-  Sentry.init();
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({
+        // to trace all requests to the default router
+        app,
+        // alternatively, you can specify the routes you want to trace:
+        // router: someRouter,
+      }),
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
   app.use(Sentry.Handlers.requestHandler());
+
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
 
   // Secure code best practice - see:
   // 1. https://expressjs.com/en/advanced/best-practice-security.html,
