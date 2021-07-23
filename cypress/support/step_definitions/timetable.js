c54baa81c38cc397c1ab023a5f8ee49ce59813f4
jonhindle <jon.hindle@digital.justice.gov.uk>
2021-07-23 16:51:50 +0300

    test/e2e_profile

--- cypress/support/step_definitions/timetable.js
+++ cypress/support/step_definitions/timetable.js
@@ -1,32 +1,9 @@
-import { And, Then, When } from 'cypress-cucumber-preprocessor/steps';
-import { activity, appointment } from '../../mockApis/data';
+import { And, Then } from 'cypress-cucumber-preprocessor/steps';
 import { horizontalTableToObject } from './utils';
-
-import { format, addDays } from 'date-fns';
 
 Then('I am displayed todays timetable', () => {
   cy.get('.todays-events > .govuk-body').contains(/.+/);
   cy.log('Timetable data has been passed through');
-});
-
-const daysFromNow = n => format(addDays(new Date(), n), 'yyyy-MM-dd');
-
-When('I have the following up coming events', args => {
-  const rows = horizontalTableToObject(args);
-
-  const events = rows.map(row => {
-    const date = row.when === 'today' ? daysFromNow(0) : daysFromNow(1);
-    return row.type === 'activity'
-      ? activity(date, row.start, row.end, row.name, row.location)
-      : appointment(date, row.start, row.end, row.name, row.location);
-  });
-
-  cy.task('stubEvents', events);
-});
-
-And('I log into the hub', () => {
-  cy.task('stubPrisonerSignIn');
-  cy.get('[data-test="signin-prompt"] > .govuk-link').click();
 });
 
 Then('I am shown my time table', args => {
