f6955ce134278a992d9b983beebb7714e0b26329
jonhindle <jon.hindle@digital.justice.gov.uk>
2021-08-06 11:21:30 +0300

    bug/git-info-not-showing-in-health

--- server/routes/health.js
+++ server/routes/health.js
@@ -1,22 +1,32 @@
 const express = require('express');
 
-const createHealthRouter = ({ buildInfo: build }) => {
+const createHealthRouter = () => {
   const router = express.Router();
 
-  router.get('/', (_, res) => res.json(addAppInfo({ healthy: true }, build)));
+  router.get('/', (_, res) => res.json(addAppInfo({ healthy: true })));
   router.get('/readiness', (_, res) => res.json({ status: 'OK' }));
 
   return router;
 };
 
-function addAppInfo(result, build) {
+function addAppInfo(result) {
+  const buildInformation = getBuild();
   const buildInfo = {
     uptime: process.uptime(),
-    build,
-    version: build && build.buildNumber,
+    build: buildInformation,
+    version: buildInformation && buildInformation.buildNumber,
   };
 
   return { ...result, ...buildInfo };
+}
+
+function getBuild() {
+  try {
+    // eslint-disable-next-line import/no-unresolved,global-require
+    return require('../../build-info.json');
+  } catch (ex) {
+    return null;
+  }
 }
 
 module.exports = {
