a279a381abaa68ed110ff5c15f67c2a9478d1f2c
David Thomas <david.thomas1@digital.justice.gov.uk>
2021-09-10 16:00:25 +0100

    Extend and refactor testing

--- server/services/__tests__/cmsService.spec.js
+++ server/services/__tests__/cmsService.spec.js
@@ -1,17 +1,26 @@
 const { CmsApi } = require('../../repositories/cmsApi');
 const {
-  BasicPageQuery,
-} = require('../../repositories/cmsQueries/basicPageQuery');
-const {
   HomepageQuery,
 } = require('../../repositories/cmsQueries/homePageQuery');
 const { TopicsQuery } = require('../../repositories/cmsQueries/topicsQuery');
+const {
+  BasicPageQuery,
+} = require('../../repositories/cmsQueries/basicPageQuery');
+const {
+  AudioPageQuery,
+} = require('../../repositories/cmsQueries/audioPageQuery');
+const {
+  VideoPageQuery,
+} = require('../../repositories/cmsQueries/videoPageQuery');
 const {
   SecondaryTagPageQuery,
 } = require('../../repositories/cmsQueries/secondaryTagPageQuery');
 const {
   SecondaryTagHeaderPageQuery,
 } = require('../../repositories/cmsQueries/secondaryTagHeaderPageQuery');
+const {
+  NextEpisodeQuery,
+} = require('../../repositories/cmsQueries/nextEpisodeQuery');
 const { CmsService } = require('../cms');
 
 jest.mock('../../repositories/cmsApi');
@@ -20,6 +29,9 @@ describe('cms Service', () => {
   const cmsApi = new CmsApi();
   let cmsService;
   let contentRepository;
+  const ESTABLISHMENT_NAME = 'wayland';
+  const SERIES_SORT_VALUE = 1001;
+  const SERIES_ID = 923;
 
   beforeEach(() => {
     contentRepository = {
@@ -68,16 +80,16 @@ describe('cms Service', () => {
       media: 'https://cms.org/jdajsgjdfj.mp3',
       programmeCode: 'FAITH138',
       seasonId: 1,
-      seriesSortValue: 1001,
+      seriesSortValue: SERIES_SORT_VALUE,
       secondaryTags: [
         {
           id: 741,
           name: 'Self-help',
         },
       ],
-      seriesId: 923,
+      seriesId: SERIES_ID,
       seriesName: 'Buddhist',
-      seriesPath: '/tags/923',
+      seriesPath: `/tags/${SERIES_ID}`,
       title: 'Buddhist reflection: 29 July',
     });
 
@@ -93,16 +105,16 @@ describe('cms Service', () => {
       },
       media: 'https://cms.org/jdajsgjdfj.mp4',
       seasonId: 1,
-      seriesSortValue: 1001,
+      seriesSortValue: SERIES_SORT_VALUE,
       secondaryTags: [
         {
           id: 741,
           name: 'Self-help',
         },
       ],
-      seriesId: 923,
+      seriesId: SERIES_ID,
       seriesName: 'Buddhist',
-      seriesPath: '/tags/923',
+      seriesPath: `/tags/${SERIES_ID}`,
       title: 'Buddhist reflection: 29 July',
     });
 
@@ -144,7 +156,7 @@ describe('cms Service', () => {
         location: 'https://cms.org/content/1234',
       });
 
-      const result = await cmsService.getContent('berwyn', 793, 1234);
+      const result = await cmsService.getContent(ESTABLISHMENT_NAME, 793, 1234);
 
       expect(result).toStrictEqual({
         id: 5923,
@@ -154,113 +166,163 @@ describe('cms Service', () => {
       });
     });
 
-    it(`returns audio content provided by CMS service`, async () => {
-      cmsApi.get
-        .mockResolvedValueOnce(createAudioPage())
-        .mockResolvedValueOnce(createNextEpisode());
-      cmsApi.lookupContent.mockResolvedValue({
-        type: 'node--moj_radio_item',
-        location: 'https://cms.org/content/1234',
+    describe(`with audio content`, () => {
+      let result;
+      beforeEach(async () => {
+        cmsApi.get
+          .mockResolvedValueOnce(createAudioPage())
+          .mockResolvedValueOnce(createNextEpisode());
+        cmsApi.lookupContent.mockResolvedValue({
+          type: 'node--moj_radio_item',
+          location: 'https://cms.org/content/1234',
+        });
+        result = await cmsService.getContent(ESTABLISHMENT_NAME, 793, 1234);
       });
-
-      const result = await cmsService.getContent('berwyn', 793, 1234);
-
-      expect(result).toStrictEqual({
-        categories: [648],
-        contentType: 'radio',
-        description: 'Education content for prisoners',
-        episodeId: 1036,
-        id: 6236,
-        image: {
-          alt: 'faith',
-          url: 'https://cms.org/jdajsgjdfj.jpg',
-        },
-        media: 'https://cms.org/jdajsgjdfj.mp3',
-        nextEpisodes: [
-          {
-            id: 1,
-            title: 'foo episode',
+      it('should make audio query', () => {
+        expect(cmsApi.get).toHaveBeenNthCalledWith(
+          1,
+          new AudioPageQuery('https://cms.org/content/1234'),
+        );
+      });
+      it('should retrieve the next episodes', () => {
+        expect(cmsApi.get).toHaveBeenNthCalledWith(
+          2,
+          new NextEpisodeQuery(
+            ESTABLISHMENT_NAME,
+            SERIES_ID,
+            SERIES_SORT_VALUE,
+          ),
+        );
+      });
+      it('should retrieve suggested content', () => {
+        expect(contentRepository.suggestedContentFor).toHaveBeenCalledWith({
+          establishmentId: 793,
+          id: 6236,
+        });
+      });
+      it('returns audio content provided by CMS service', async () => {
+        expect(result).toStrictEqual({
+          categories: [648],
+          contentType: 'radio',
+          description: 'Education content for prisoners',
+          episodeId: 1036,
+          id: 6236,
+          image: {
+            alt: 'faith',
+            url: 'https://cms.org/jdajsgjdfj.jpg',
           },
-          {
-            id: 2,
-            title: 'bar episode',
-          },
-        ],
-        programmeCode: 'FAITH138',
-        seasonId: 1,
-        seriesSortValue: 1001,
-        secondaryTags: [
-          {
-            id: 741,
-            name: 'Self-help',
-          },
-        ],
-        seriesId: 923,
-        seriesName: 'Buddhist',
-        seriesPath: '/tags/923',
-        suggestedContent: [
-          {
-            href: 'www.foo.com',
-            title: 'foo',
-            type: 'foo',
-          },
-        ],
-        title: 'Buddhist reflection: 29 July',
+          media: 'https://cms.org/jdajsgjdfj.mp3',
+          nextEpisodes: [
+            {
+              id: 1,
+              title: 'foo episode',
+            },
+            {
+              id: 2,
+              title: 'bar episode',
+            },
+          ],
+          programmeCode: 'FAITH138',
+          seasonId: 1,
+          seriesSortValue: SERIES_SORT_VALUE,
+          secondaryTags: [
+            {
+              id: 741,
+              name: 'Self-help',
+            },
+          ],
+          seriesId: SERIES_ID,
+          seriesName: 'Buddhist',
+          seriesPath: `/tags/${SERIES_ID}`,
+          suggestedContent: [
+            {
+              href: 'www.foo.com',
+              title: 'foo',
+              type: 'foo',
+            },
+          ],
+          title: 'Buddhist reflection: 29 July',
+        });
       });
     });
 
-    it(`returns video content provided by CMS service`, async () => {
-      cmsApi.get
-        .mockResolvedValueOnce(createVideoPage())
-        .mockResolvedValueOnce(createNextEpisode());
-      cmsApi.lookupContent.mockResolvedValue({
-        type: 'node--moj_video_item',
-        location: 'https://cms.org/content/1234',
+    describe(`with video content`, () => {
+      let result;
+      beforeEach(async () => {
+        cmsApi.get
+          .mockResolvedValueOnce(createVideoPage())
+          .mockResolvedValueOnce(createNextEpisode());
+        cmsApi.lookupContent.mockResolvedValue({
+          type: 'node--moj_video_item',
+          location: 'https://cms.org/content/1234',
+        });
+        result = await cmsService.getContent(ESTABLISHMENT_NAME, 793, 1234);
       });
+      it('should make video query', () => {
+        expect(cmsApi.get).toHaveBeenNthCalledWith(
+          1,
+          new VideoPageQuery('https://cms.org/content/1234'),
+        );
+      });
+      it('should retrieve the next episodes', () => {
+        expect(cmsApi.get).toHaveBeenNthCalledWith(
+          2,
+          new NextEpisodeQuery(
+            ESTABLISHMENT_NAME,
+            SERIES_ID,
+            SERIES_SORT_VALUE,
+          ),
+        );
+      });
+      it('should retrieve suggested content', () => {
+        expect(contentRepository.suggestedContentFor).toHaveBeenCalledWith({
+          establishmentId: 793,
+          id: 6236,
+        });
+      });
+      it('returns video content provided by CMS service', async () => {
+        expect(result).toStrictEqual({
+          categories: [648],
+          contentType: 'video',
+          description: 'Education content for prisoners',
+          episodeId: 1036,
+          id: 6236,
+          image: {
+            alt: 'faith',
+            url: 'https://cms.org/jdajsgjdfj.jpg',
+          },
+          media: 'https://cms.org/jdajsgjdfj.mp4',
+          nextEpisodes: [
+            {
+              id: 1,
+              title: 'foo episode',
+            },
+            {
+              id: 2,
+              title: 'bar episode',
+            },
+          ],
 
-      const result = await cmsService.getContent('berwyn', 793, 1234);
-
-      expect(result).toStrictEqual({
-        categories: [648],
-        contentType: 'video',
-        description: 'Education content for prisoners',
-        episodeId: 1036,
-        id: 6236,
-        image: {
-          alt: 'faith',
-          url: 'https://cms.org/jdajsgjdfj.jpg',
-        },
-        media: 'https://cms.org/jdajsgjdfj.mp4',
-        nextEpisodes: [
-          {
-            id: 1,
-            title: 'foo episode',
-          },
-          {
-            id: 2,
-            title: 'bar episode',
-          },
-        ],
-
-        seasonId: 1,
-        seriesSortValue: 1001,
-        secondaryTags: [
-          {
-            id: 741,
-            name: 'Self-help',
-          },
-        ],
-        seriesId: 923,
-        seriesName: 'Buddhist',
-        seriesPath: '/tags/923',
-        suggestedContent: [
-          {
-            href: 'www.foo.com',
-            title: 'foo',
-            type: 'foo',
-          },
-        ],
-        title: 'Buddhist reflection: 29 July',
+          seasonId: 1,
+          seriesSortValue: SERIES_SORT_VALUE,
+          secondaryTags: [
+            {
+              id: 741,
+              name: 'Self-help',
+            },
+          ],
+          seriesId: SERIES_ID,
+          seriesName: 'Buddhist',
+          seriesPath: `/tags/${SERIES_ID}`,
+          suggestedContent: [
+            {
+              href: 'www.foo.com',
+              title: 'foo',
+              type: 'foo',
+            },
+          ],
+          title: 'Buddhist reflection: 29 July',
+        });
       });
     });
 
@@ -302,7 +364,7 @@ describe('cms Service', () => {
     it('returns topics', async () => {
       cmsApi.get.mockResolvedValue([createTopic('topic')]);
 
-      const result = await cmsService.getTopics('berwyn');
+      const result = await cmsService.getTopics(ESTABLISHMENT_NAME);
 
       expect(result).toStrictEqual([
         {
@@ -317,9 +379,11 @@ describe('cms Service', () => {
     it('Source to have been called correctly', async () => {
       cmsApi.get.mockResolvedValue([]);
 
-      await cmsService.getTopics('berwyn');
+      await cmsService.getTopics(ESTABLISHMENT_NAME);
 
-      expect(cmsApi.get).toHaveBeenCalledWith(new TopicsQuery('berwyn'));
+      expect(cmsApi.get).toHaveBeenCalledWith(
+        new TopicsQuery(ESTABLISHMENT_NAME),
+      );
     });
   });
 
@@ -368,7 +432,7 @@ describe('cms Service', () => {
     it('returns homepage content', async () => {
       cmsApi.get.mockResolvedValue([homepage]);
 
-      const result = await cmsService.getHomepage('berwyn');
+      const result = await cmsService.getHomepage(ESTABLISHMENT_NAME);
 
       expect(result).toStrictEqual({
         upperFeatured: {
@@ -415,13 +479,14 @@ describe('cms Service', () => {
     it('Source to have been called correctly', async () => {
       cmsApi.get.mockResolvedValue([]);
 
-      await cmsService.getHomepage('berwyn');
+      await cmsService.getHomepage(ESTABLISHMENT_NAME);
 
-      expect(cmsApi.get).toHaveBeenCalledWith(new HomepageQuery('berwyn'));
+      expect(cmsApi.get).toHaveBeenCalledWith(
+        new HomepageQuery(ESTABLISHMENT_NAME),
+      );
     });
   });
   describe('getTag', () => {
-    const ESTABLISHMENT_NAME = 'Wayland';
     const TAG_ID = 9;
     describe('with a secondary tag', () => {
       const uuid = 42;
