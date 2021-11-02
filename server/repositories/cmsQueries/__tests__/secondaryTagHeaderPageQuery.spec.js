6cd45daa10e7698b7b3be8797dafc94eee4c47fe
jonhindle <50403492+jonhindle@users.noreply.github.com>
2021-11-02 14:20:26 +0000

    Feature/switch to jsonapi category landing pages (#383)
    * handle-duplicates-in-query
    
    * feature/switch-to-jsonapi-for-you-might-like
    
    * handle-duplicates-in-query
    
    * new_single_suggestions_endpoint
    
    * feature/switch-to-jsonapi-for-you-might-like
    
    * handle-duplicates-in-query
    
    * handle-duplicates-in-query
    
    * category-tests-fixes
    
    * in-this-section
    
    * use-path-remove-duplication
    
    * bug/add-query-limit-fix-series-images
    
    * bug/add-query-limit-fix-series-images

--- server/repositories/cmsQueries/__tests__/secondaryTagHeaderPageQuery.spec.js
+++ server/repositories/cmsQueries/__tests__/secondaryTagHeaderPageQuery.spec.js
@@ -7,7 +7,7 @@ describe('Secondary tag with no defined related content page query', () => {
   describe('url', () => {
     it('should create the correct url', async () => {
       expect(query.url()).toStrictEqual(
-        'https://cms/content/1234?include=field_featured_image&fields%5Btaxonomy_term--tags%5D=name%2Cdescription%2Cdrupal_internal__tid%2Cfield_featured_image',
+        'https://cms/content/1234?include=field_featured_image&fields%5Btaxonomy_term--tags%5D=name%2Cdescription%2Cdrupal_internal__tid%2Cfield_featured_image%2Cpath',
       );
     });
   });
@@ -17,22 +17,25 @@ describe('Secondary tag with no defined related content page query', () => {
       const page = {
         drupalInternal_Tid: `1001`,
         name: `name1`,
+        type: 'taxonomy_term--tags',
         description: { processed: `description1` },
         fieldFeaturedImage: {
           imageStyleUri: [{ tile_large: `tile_large1` }],
           resourceIdObjMeta: { alt: `alt1` },
         },
+        path: { alias: 'tags/1' },
       };
 
       expect(query.transform(page)).toStrictEqual({
         id: `1001`,
         contentType: 'tags',
-        name: `name1`,
-        description: `description1`,
+        title: `name1`,
+        summary: `description1`,
         image: {
           url: `tile_large1`,
           alt: `alt1`,
         },
+        contentUrl: 'tags/1',
       });
     });
   });
