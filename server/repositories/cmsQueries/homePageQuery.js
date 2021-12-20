4c689de59ad053a7f9d1da9f3b7a223de154e0bb
David Thomas <david.thomas1@digital.justice.gov.uk>
2021-12-20 16:47:38 +0000

    feature/enable-homepage-series-tiles

--- server/repositories/cmsQueries/homePageQuery.js
+++ server/repositories/cmsQueries/homePageQuery.js
@@ -4,8 +4,9 @@ const { getLargeTile, getSmallTile } = require('../../utils/jsonApi');
 class HomepageQuery {
   static #TILE_FIELDS = [
     'drupal_internal__nid',
-    'field_moj_thumbnail_image',
     'title',
+    'field_moj_thumbnail_image',
+    'field_thumbnail_image',
     'field_moj_description',
     'field_moj_series',
     'path',
@@ -18,13 +19,21 @@ class HomepageQuery {
       .addFields('node--featured_articles', [
         'title',
         'drupal_internal__nid',
-        'field_moj_featured_tile_large',
-        'field_moj_featured_tile_small',
+        'field_featured_tile_large',
+        'field_featured_tile_small',
       ])
       .addFields('node--page', HomepageQuery.#TILE_FIELDS)
       .addFields('node--moj_video_item', HomepageQuery.#TILE_FIELDS)
       .addFields('node--moj_radio_item', HomepageQuery.#TILE_FIELDS)
       .addFields('node--moj_pdf_item', HomepageQuery.#TILE_FIELDS)
+      .addFields('taxonomy_term--series', [
+        'drupal_internal__tid',
+        'name',
+        'description',
+        'path',
+        'field_featured_image',
+      ])
+
       .addFields('file--file', [
         'drupal_internal__fid',
         'id',
@@ -32,8 +41,10 @@ class HomepageQuery {
       ])
 
       .addInclude([
-        'field_moj_featured_tile_large.field_moj_thumbnail_image',
-        'field_moj_featured_tile_small.field_moj_thumbnail_image',
+        'field_featured_tile_large.field_moj_thumbnail_image',
+        'field_featured_tile_small.field_moj_thumbnail_image',
+        'field_featured_tile_large.field_featured_image',
+        'field_featured_tile_small.field_featured_image',
       ])
       .addPageLimit(1)
       .getQueryString();
@@ -44,13 +55,13 @@ class HomepageQuery {
   }
 
   transformEach(item) {
-    const [upperFeatured, lowerFeatured] = item.fieldMojFeaturedTileLarge.map(
+    const [upperFeatured, lowerFeatured] = item.fieldFeaturedTileLarge.map(
       featured => (featured?.path?.alias ? getLargeTile(featured) : null),
     );
     return {
       upperFeatured,
       lowerFeatured,
-      smallTiles: item.fieldMojFeaturedTileSmall
+      smallTiles: item.fieldFeaturedTileSmall
         .filter(({ title = null, name = null }) => title || name)
         .map(getSmallTile),
     };
