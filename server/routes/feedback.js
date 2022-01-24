3da71c323a40fef9a62ba799be0efe068094057c
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2022-01-24 14:35:16 +0000

    Log user id of those making feedback statements (#417)

--- server/routes/feedback.js
+++ server/routes/feedback.js
@@ -1,24 +1,35 @@
-const { path } = require('ramda');
 const express = require('express');
+const { logger } = require('../utils/logger');
 
 const createFeedbackRouter = ({ feedbackService }) => {
   const router = express.Router();
 
   router.post('/:feedbackId', (req, res) => {
-    const sessionId = path(['session', 'id'], req);
+    const {
+      body,
+      params: { feedbackId } = {},
+      session: { id: sessionId } = {},
+      user,
+    } = req;
+
+    logger.info(
+      `Prisoner '${
+        user?.prisonerId || 'anon'
+      }' leaving feedback: ${feedbackId}`,
+    );
 
     feedbackService.sendFeedback({
-      title: path(['body', 'title'], req),
-      url: path(['body', 'url'], req),
-      contentType: path(['body', 'contentType'], req),
-      feedbackId: path(['params', 'feedbackId'], req),
-      series: path(['body', 'series'], req),
-      categories: path(['body', 'categories'], req),
-      secondaryTags: path(['body', 'secondaryTags'], req),
-      sentiment: path(['body', 'sentiment'], req),
-      comment: path(['body', 'comment'], req),
+      title: body?.title,
+      url: body?.url,
+      contentType: body?.contentType,
+      feedbackId,
+      series: body?.series,
+      categories: body?.categories,
+      secondaryTags: body?.secondaryTags,
+      sentiment: body?.sentiment,
+      comment: body?.comment,
       date: new Date().toISOString(),
-      establishment: path(['session', 'establishmentName'], req).toUpperCase(),
+      establishment: req.session?.establishmentName?.toUpperCase(),
       sessionId,
     });
 
