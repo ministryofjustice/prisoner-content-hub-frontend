82e0cd8534a6519cb681bbe7d7ad07fa63243b31
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2022-03-29 16:20:17 +0100

    Sanitising and limiting search input

--- server/services/search.js
+++ server/services/search.js
@@ -1,23 +1,28 @@
 const { SearchQuery } = require('../repositories/cmsQueries/searchQuery');
 
 const createSearchService = ({ cmsApi }) => {
+  const isInvalid = query => query === '' || query.length > 50;
+  const sanitise = query => query.replace(/[^a-zA-Z0-9';,\-()!"]+/g, ' ');
+
   function find(query, establishmentName) {
-    if (query === '') {
+    if (isInvalid(query)) {
       return [];
     }
 
-    return cmsApi.get(new SearchQuery(establishmentName, query, 15));
+    return cmsApi.get(new SearchQuery(establishmentName, sanitise(query), 15));
   }
 
   function typeAhead(query, establishmentName) {
-    if (query === '') {
+    if (isInvalid(query)) {
       return [];
     }
 
-    return cmsApi.get(new SearchQuery(establishmentName, query, 5));
+    return cmsApi.get(new SearchQuery(establishmentName, sanitise(query), 5));
   }
 
   return {
+    isInvalid,
+    sanitise,
     find,
     typeAhead,
   };
