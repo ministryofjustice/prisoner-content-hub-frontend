474879a630c1f5be5b93b8ad953b7095de1a5cef
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2022-03-30 08:12:19 +0100

    Sanitising and limiting search input (#459)

--- server/services/__tests__/search.spec.js
+++ server/services/__tests__/search.spec.js
@@ -37,6 +37,16 @@ describe('SearchService', () => {
       expect(results.length).toBe(0);
       expect(cmsApi.get).not.toHaveBeenCalled();
     });
+
+    it('returns empty when invalid', async () => {
+      cmsApi.get.mockResolvedValue([]);
+
+      const results = await service.find('a'.repeat(51), 'wayland');
+
+      expect(Array.isArray(results)).toBe(true);
+      expect(results.length).toBe(0);
+      expect(cmsApi.get).not.toHaveBeenCalled();
+    });
   });
 
   describe('#typeAhead', () => {
@@ -62,6 +72,51 @@ describe('SearchService', () => {
       expect(cmsApi.get).not.toHaveBeenCalled();
       expect(Array.isArray(results)).toBe(true);
       expect(results.length).toBe(0);
+    });
+
+    it('returns empty when invalid', async () => {
+      cmsApi.get.mockResolvedValue([]);
+
+      const results = await service.typeAhead('a'.repeat(51), 'wayland');
+
+      expect(cmsApi.get).not.toHaveBeenCalled();
+      expect(Array.isArray(results)).toBe(true);
+      expect(results.length).toBe(0);
+    });
+  });
+
+  describe('#isInvalid', () => {
+    ['a', 'a hat', 'a'.repeat(50)].forEach(test => {
+      it(`${test} should be valid input to search`, () => {
+        expect(service.isInvalid(test)).toBe(false);
+      });
+    });
+    ['', 'a'.repeat(51)].forEach(test => {
+      it(`'${test}' should be valid input to search`, () => {
+        expect(service.isInvalid(test)).toBe(true);
+      });
+    });
+  });
+
+  describe('#sanitise', () => {
+    [
+      ['a', 'a'],
+      ['a hat', 'a hat'],
+      ["Somebody's hat", "Somebody's hat"],
+      ['The: thing', 'The thing'],
+      ['The / thing', 'The thing'],
+      ['The (large) thing', 'The (large) thing'],
+      ['Weird ~ stuff', 'Weird stuff'],
+      ['Weird + stuff', 'Weird stuff'],
+      ['fine-stuff', 'fine-stuff'],
+      ['fine,', 'fine,'],
+      ['fi;ne', 'fi;ne'],
+      ['fine!', 'fine!'],
+      ['"fine"', '"fine"'],
+    ].forEach(([test, expected]) => {
+      it(`'${test}' should be sanitised to '${expected}'`, () => {
+        expect(service.sanitise(test)).toBe(expected);
+      });
     });
   });
 });
