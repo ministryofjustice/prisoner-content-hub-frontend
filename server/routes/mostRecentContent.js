78440c0ebfda5ba8e7f592b30909fcff8a80d9f2
Barry Gee <barry.gee@digital.justice.gov.uk>
2022-05-09 15:28:36 +0100

    Add createMostRecentContentRouter

---
+++ server/routes/mostRecentContent.js
@@ -0,0 +1,37 @@
+const express = require('express');
+// const { groupBy } = require('../utils');
+
+const createMostRecentContentRouter = ({ cmsService }) => {
+  const router = express.Router();
+
+  router.get('/', async (req, res, next) => {
+    try {
+      const { establishmentName } = req.session;
+
+      if (!establishmentName) {
+        throw new Error('Could not determine establishment!');
+      }
+
+      const data = await cmsService.getMostRecentContent(
+        establishmentName,
+        1,
+        10,
+      );
+
+      res.render('pages/mostRecentContent', {
+        title: 'Recently added',
+        data,
+        breadcrumbs: ['test'],
+      });
+    } catch (e) {
+      e.message = `Error loading topics: ${e.message}`;
+      next(e);
+    }
+  });
+
+  return router;
+};
+
+module.exports = {
+  createMostRecentContentRouter,
+};
