73557aadcecf0dcbbc2d4568e0da580154b928e2
Barry Gee <barry.gee@digital.justice.gov.uk>
2022-05-12 14:44:22 +0100

    Update route to use json param

--- server/routes/tags.js
+++ server/routes/tags.js
@@ -3,7 +3,7 @@ const express = require('express');
 const createTagRouter = ({ cmsService }) => {
   const router = express.Router();
 
-  router.get('/:id.json', async (req, res, next) => {
+  router.get('/:id/json', async (req, res, next) => {
     try {
       const { id } = req.params;
       const { page, pageType } = req.query;
