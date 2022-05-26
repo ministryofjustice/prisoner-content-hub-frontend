a7f8646df782a71bcae0d180b55dc2b8b339a174
Barry Gee <barry.gee@digital.justice.gov.uk>
2022-05-26 11:27:57 +0100

    add new homepage route

--- server/routes/homepage.js
+++ server/routes/homepage.js
@@ -34,6 +34,37 @@ const createHomepageRouter = ({ cmsService, offenderService }) => {
     }
   });
 
+  router.get('/new-homepage', async (req, res, next) => {
+    try {
+      const { establishmentName } = req.session;
+
+      if (!establishmentName) {
+        throw new Error('Could not determine establishment!');
+      }
+
+      const homepage = await cmsService.getHomepage(establishmentName);
+
+      const currentEvents = res.locals.isSignedIn
+        ? await offenderService.getCurrentEvents(req.user)
+        : {};
+
+      res.render('pages/home-new', {
+        config: {
+          content: true,
+          header: true,
+          postscript: true,
+          detailsType: 'large',
+        },
+        hideSignInLink: true,
+        title: 'Home',
+        homepage,
+        currentEvents,
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
   return router;
 };
 
