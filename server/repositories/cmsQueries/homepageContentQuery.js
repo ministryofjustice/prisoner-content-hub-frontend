b044c2361c673573989570f42fa1ad4ddd71e62e
Barry Gee <barry.gee@digital.justice.gov.uk>
2022-06-30 12:21:37 +0100

    commit

--- server/repositories/cmsQueries/homepageContentQuery.js
+++ server/repositories/cmsQueries/homepageContentQuery.js
@@ -1,5 +1,9 @@
 const { DrupalJsonApiParams: Query } = require('drupal-jsonapi-params');
-const { getSmallTile, getLargeTile } = require('../../utils/jsonApi');
+const {
+  getSmallTile,
+  getLargeTile,
+  cropTextWithEllipsis,
+} = require('../../utils/jsonApi');
 
 class HomepageContentQuery {
   static #TILE_FIELDS = [
@@ -19,6 +23,11 @@ class HomepageContentQuery {
 
       .addFields(
         'node--field_featured_tiles',
+        HomepageContentQuery.#TILE_FIELDS,
+      )
+
+      .addFields(
+        'node--field_key_info_tiles',
         HomepageContentQuery.#TILE_FIELDS,
       )
 
@@ -53,7 +62,8 @@ class HomepageContentQuery {
       keyInfo: {
         data: item.fieldKeyInfoTiles
           .filter(({ title = null, name = null }) => title || name)
-          .map(getSmallTile),
+          .map(getSmallTile)
+          .map(keyInfoItem => cropTextWithEllipsis(keyInfoItem, 30)),
       },
       largeUpdateTile: item?.fieldLargeUpdateTile
         ? getLargeTile(item.fieldLargeUpdateTile)
