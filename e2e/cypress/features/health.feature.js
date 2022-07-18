bbb844ad8851134748da7053eabeb3fbc660dab6
jonhindle <jon.hindle@digital.justice.gov.uk>
2022-07-18 14:14:48 +0100

    test/1165-health-feature-remove-cucumber

---
+++ e2e/cypress/features/health.feature.js
@@ -0,0 +1,15 @@
+context('Healthcheck', () => {
+  context('All healthy', () => {
+    beforeEach(() => {
+      cy.task('reset');
+    });
+
+    it('Health check page is visible', () => {
+      cy.request('/health').its('body.healthy').should('equal', true);
+    });
+
+    it('Health/readiness is visible and UP', () => {
+      cy.request('/health/readiness').its('body.status').should('equal', 'OK');
+    });
+  });
+});
