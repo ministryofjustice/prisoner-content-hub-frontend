9b17611b454135e5b2bdc272c7fab009ffc19077
Barry Gee <barry.gee@digital.justice.gov.uk>
2023-02-09 08:10:29 +0000

    add env variables for feature toggle and test condition in view
    Update feature toggle variable
    
    add establishment restriction to the adjudications feature
    
    add placeholder adjudications section on my profile

--- server/config.js
+++ server/config.js
@@ -85,6 +85,8 @@ module.exports = {
     useRedisCache: getEnv('ENABLE_REDIS_CACHE', 'true') === 'true',
     approvedVisitorsFeatureEnabled:
       getEnv('APPROVED_VISITORS_FEATURE_ENABLED', 'false') === 'true',
+    adjudicationsFeatureEnabled:
+      getEnv('ADJUDICATIONS_FEATURE_ENABLED', 'false') === 'true',
   },
   analytics: {
     endpoint: getEnv(
