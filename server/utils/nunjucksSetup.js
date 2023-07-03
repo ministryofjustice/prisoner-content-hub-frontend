f4c592834aa917b74f3ca99f62f2855e9587514a
BarryGeeMOJ <104000682+BarryGeeMOJ@users.noreply.github.com>
2023-07-03 07:17:24 +0100

    Remove UA add GA4 (#661)
    * Remove UA and GA4
    
    * Add newline at EOF

--- server/utils/nunjucksSetup.js
+++ server/utils/nunjucksSetup.js
@@ -18,6 +18,7 @@ module.exports = expressApp => {
   });
 
   nunjucksEnv.addGlobal('knownPages', knownPages);
+  nunjucksEnv.addGlobal('GA4SiteId', analytics.siteId);
   nunjucksEnv.addGlobal('GTMSiteId', analytics.gtmSiteId);
   nunjucksEnv.addFilter('skip', (array, count) => array.slice(count));
 
