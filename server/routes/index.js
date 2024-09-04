cc013576fc1ac1f8fd6ae00fa528f999f0d3ff18
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2024-09-04 14:39:52 +0100

    fix commit errors

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,6 @@ const { createHomepageRouter } = require('./homepage');
 const { createTopicsRouter } = require('./topics');
 const { createTimetableRouter } = require('./timetable');
 const { createContentRouter } = require('./content');
-const { createMoneyRouter } = require('./money');
-const { createAdjudicationsRouter } = require('./adjudications');
-const { createApprovedVisitorsRouter } = require('./approvedVisitors');
-const { createProfileRouter } = require('./profile');
 const { createTagRouter } = require('./tags');
 const { createLinkRouter } = require('./link');
 const { createGamesRouter } = require('./games');
@@ -26,7 +22,6 @@ module.exports = (
     logger,
     cmsService,
     offenderService,
-    prisonerInformationService,
     searchService,
     feedbackService,
     config,
@@ -75,37 +70,9 @@ module.exports = (
   router.use('/timetable', createTimetableRouter({ offenderService }));
 
   router.use(
-    '/money',
-    createMoneyRouter({
-      prisonerInformationService,
-    }),
-  );
-
-  router.use(
-    '/approved-visitors',
-    createApprovedVisitorsRouter({
-      offenderService,
-    }),
-  );
-
-  router.use(
-    '/profile',
-    createProfileRouter({
-      offenderService,
-    }),
-  );
-
-  router.use(
     '/content',
     createContentRouter({
       cmsService,
-    }),
-  );
-
-  router.use(
-    '/adjudications',
-    createAdjudicationsRouter({
-      offenderService,
     }),
   );
 
