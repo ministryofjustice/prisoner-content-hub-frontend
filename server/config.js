648bbeeb066139afdc590eeb851f9913b80dca20
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2024-09-05 15:33:58 +0100

    revert config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,10 @@
 const { getEnv, getRequiredEnv, isProduction } = require('../utils/index');
 
 const hubEndpoint = getRequiredEnv('HUB_API_ENDPOINT', 'http://localhost:9090');
+const hmppsAuthBaseUrl = getRequiredEnv(
+  'HMPPS_AUTH_BASE_URL',
+  'https://api.nomis',
+);
 const elasticsearchEndpoint = getRequiredEnv(
   'ELASTICSEARCH_ENDPOINT',
   'http://localhost:9200',
@@ -9,6 +13,16 @@ const feedbackEndpoint = getRequiredEnv(
   'FEEDBACK_ENDPOINT',
   '/local-feedback/_doc',
 );
+
+// covers all features being enabled to stop repetition as requested, commented out to stop errors in linting while no sites are using everything
+// const ALL_PROFILE_FEATURES = [
+//   'adjudications',
+//   'approvedVisitors',
+//   'incentives',
+//   'money',
+//   'timetable',
+//   'visits',
+// ];
 
 module.exports = {
   isProduction,
@@ -43,11 +57,136 @@ module.exports = {
     password: process.env.REDIS_AUTH_TOKEN,
     tls_enabled: getEnv('REDIS_TLS_ENABLED', 'false'),
   },
+  prisonApi: {
+    auth: {
+      clientId: getEnv('HMPPS_AUTH_CLIENT_ID', 'UNSET'),
+      clientSecret: getEnv('HMPPS_AUTH_CLIENT_SECRET', 'UNSET'),
+      authUrl: `${hmppsAuthBaseUrl}/oauth/token?grant_type=client_credentials`,
+    },
+    baseUrl: getRequiredEnv('PRISON_API_BASE_URL', 'https://api.nomis'),
+    adjudications: {
+      pageLimit: 50,
+      maxAdjudicationsPerPage: 10,
+    },
+  },
+  prisonerContactRegistryApi: {
+    auth: {
+      clientId: getEnv('HMPPS_AUTH_CLIENT_ID', 'UNSET'),
+      clientSecret: getEnv('HMPPS_AUTH_CLIENT_SECRET', 'UNSET'),
+      authUrl: `${hmppsAuthBaseUrl}/oauth/token?grant_type=client_credentials`,
+    },
+    baseUrl: getRequiredEnv(
+      'PRISONER_CONTACT_REGISTRY_BASE_URL',
+      'http://localhost:8080',
+    ),
+  },
+  incentivesApi: {
+    auth: {
+      clientId: getEnv('HMPPS_AUTH_CLIENT_ID', 'UNSET'),
+      clientSecret: getEnv('HMPPS_AUTH_CLIENT_SECRET', 'UNSET'),
+      authUrl: `${hmppsAuthBaseUrl}/oauth/token?grant_type=client_credentials`,
+    },
+    baseUrl: getRequiredEnv('INCENTIVES_API_BASE_URL', 'https://api.nomis'),
+  },
+  adjudicationsApi: {
+    auth: {
+      clientId: getEnv('HMPPS_AUTH_CLIENT_ID', 'UNSET'),
+      clientSecret: getEnv('HMPPS_AUTH_CLIENT_SECRET', 'UNSET'),
+      authUrl: `${hmppsAuthBaseUrl}/oauth/token?grant_type=client_credentials`,
+    },
+    baseUrl: getRequiredEnv('ADJUDICATIONS_API_BASE_URL', 'https://api.nomis'),
+    adjudications: {
+      pageLimit: 50,
+      maxAdjudicationsPerPage: 10,
+    },
+  },
   features: {
     useMockAuth: getEnv('ENABLE_MOCK_AUTH', 'false') === 'true',
     showStackTraces:
       getEnv('ENABLE_STACK_TRACES_ON_ERROR_PAGES', 'false') === 'true',
     useRedisCache: getEnv('ENABLE_REDIS_CACHE', 'true') === 'true',
+  },
+  sites: {
+    berwyn: {
+      enabled: false,
+      features: [],
+    },
+    bullingdon: {
+      enabled: true,
+      features: ['incentives', 'money', 'timetable', 'visits'],
+    },
+    chelmsford: {
+      enabled: true,
+      features: ['incentives', 'money', 'timetable', 'visits'],
+    },
+    cookhamwood: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    erlestoke: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    felthama: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    felthamb: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    garth: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    lindholme: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    newhall: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    ranby: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    stokeheath: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    styal: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    swaleside: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    themount: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    thestudio: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    wayland: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    werrington: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    wetherby: {
+      enabled: true,
+      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+    },
+    woodhill: {
+      enabled: true,
+      features: ['incentives', 'money', 'timetable', 'visits'],
+    },
   },
   analytics: {
     endpoint: getEnv(
