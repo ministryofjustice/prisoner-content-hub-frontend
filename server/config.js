c69f1a89388c73f446f6e090a34803e2666a1160
Elliot Ward <elliot.ward@digital.justice.gov.uk>
2025-02-27 11:55:18 +0000

    LNP-1091: 🔧  remove profile from remaining sites except Cardiff and Bullingdon.

--- server/config.js
+++ server/config.js
@@ -1,6 +1,9 @@
 const { getEnv, getRequiredEnv, isProduction } = require('../utils/index');
 
-const hubEndpoint = getRequiredEnv('HUB_API_ENDPOINT', 'http://localhost:9090');
+const hubEndpoint = getRequiredEnv(
+  'HUB_API_ENDPOINT',
+  'http://localhost:11001',
+);
 const hmppsAuthBaseUrl = getRequiredEnv(
   'HMPPS_AUTH_BASE_URL',
   'https://api.nomis',
@@ -128,13 +131,13 @@ module.exports = {
       languages: ['en'],
     },
     chelmsford: {
-      enabled: true,
-      features: ['incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en'],
     },
     cookhamwood: {
-      enabled: true,
-      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en'],
     },
     erlestoke: {
@@ -157,13 +160,13 @@ module.exports = {
       languages: ['en'],
     },
     garth: {
-      enabled: true,
-      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en'],
     },
     lindholme: {
-      enabled: true,
-      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en'],
     },
     newhall: {
@@ -172,13 +175,13 @@ module.exports = {
       languages: ['en'],
     },
     ranby: {
-      enabled: true,
-      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en'],
     },
     stokeheath: {
-      enabled: true,
-      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en'],
     },
     styal: {
@@ -187,23 +190,23 @@ module.exports = {
       languages: ['en'],
     },
     swaleside: {
-      enabled: true,
-      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en'],
     },
     themount: {
-      enabled: true,
-      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en'],
     },
     thestudio: {
-      enabled: true,
-      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en', 'cy'],
     },
     wayland: {
-      enabled: true,
-      features: ['adjudications', 'incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en'],
     },
     werrington: {
@@ -217,8 +220,8 @@ module.exports = {
       languages: ['en'],
     },
     woodhill: {
-      enabled: true,
-      features: ['incentives', 'money', 'timetable', 'visits'],
+      enabled: false,
+      features: [],
       languages: ['en'],
     },
   },
