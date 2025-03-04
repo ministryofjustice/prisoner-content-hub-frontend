72e29de8ca992b55b66bfcb165452a6bbb3f22ea
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-03-04 13:32:47 +0000

    Fix SSL

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
+const { readFileSync } = require('node:fs');
+const path = require('node:path');
 const config = require('./server/config');
-// const { readFileSync } = require('node:fs')
-// const path = require('node:path')
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
+let ssl = true;
+
+if (config.isProduction === false) {
+  ssl = {
+    rejectUnauthorized: false,
+    cert: readFileSync(path.join(__dirname, './global-bundle.pem')),
+  };
+}
+
 module.exports = {
   feedback: {
     client: 'pg',
@@ -15,10 +21,7 @@ module.exports = {
       user: config.feedback.user,
       password: config.feedback.password,
       database: config.feedback.database,
-      ssl: true,
-      // ssl: {
-      //   ca: readFileSync(path.join(__dirname, '../ca-certificate.crt')),
-      // },
+      ssl,
     },
   },
 };
