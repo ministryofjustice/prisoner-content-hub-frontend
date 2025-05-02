45eb7f29e0d5f8c9a9ad27a08458105aa4a8e451
Elliot Ward <elliot.ward@digital.justice.gov.uk>
2025-05-02 16:00:37 +0100

    LNP-512: 🔥  remove sortByDateTime().

--- server/utils/date.js
+++ server/utils/date.js
@@ -15,19 +15,10 @@ const formatTimeBetweenOrDefault = (placeHolder, start, finish) => {
     : formatDistance(parseISO(start), new Date());
 };
 
-const sortByDateTime = (firstDate, secondDate) => {
-  if (firstDate && secondDate)
-    return parseISO(firstDate).valueOf() - parseISO(secondDate).valueOf();
-  if (firstDate) return -1;
-  if (secondDate) return 1;
-  return 0;
-};
-
 const getOffsetUnixTime = (offset, timeStamp) =>
   getUnixTime(subDays(timeStamp || new Date(), offset || 0));
 
 module.exports = {
   formatTimeBetweenOrDefault,
-  sortByDateTime,
   getOffsetUnixTime,
 };
