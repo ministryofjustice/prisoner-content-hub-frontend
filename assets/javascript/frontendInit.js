8c478165ec250209f24f6574d827a46858732ed2
Elliot Ward <elliot.ward@digital.justice.gov.uk>
2025-10-13 11:47:25 +0100

    LNP-1331: 🐛  reintroduce event handlers for back and forward links. (#820)

--- assets/javascript/frontendInit.js
+++ assets/javascript/frontendInit.js
@@ -3,3 +3,17 @@ import * as mojFrontend from '/public/moj/moj-frontend.min.js';
 
 govukFrontend.initAll();
 mojFrontend.initAll();
+
+(function () {
+  document.body.addEventListener('click', function (event) {
+    if (event.target.matches('#go-back')) {
+      event.preventDefault();
+      window.history.go(-1);
+    }
+
+    if (event.target.matches('#go-forwards')) {
+      event.preventDefault();
+      window.history.go(1);
+    }
+  });
+})();
