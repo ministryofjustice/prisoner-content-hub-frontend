2205faa5ead037e4c2f7c002ad3ab800381122b6
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2025-12-08 10:36:07 +0000

    update npm script allowlist

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,5 +2,11 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
    allowlist: {
+     // Needed for watching files during development
+     "node_modules/@parcel/watcher@2.5.1": "ALLOW",
+     // Provides native integration, supporting the ability to write dtrace probes for bunyan
+     "node_modules/dtrace-provider@0.8.8": "ALLOW",
+     // Needed by jest for running tests in watch mode
+     "node_modules/fsevents@2.3.3": "ALLOW"
    },
 })
